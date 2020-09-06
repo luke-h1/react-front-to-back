7f02170a3e731d9ba17191aaa4e8f98a6568b02c
luke-h1 <luke.howsam@yahoo.com>
2020-09-06 15:24:14 +0100

    update to proj

--- section-2-github-finder-react-setup/src/App.js
+++ section-2-github-finder-react-setup/src/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Navbar from './components/layout/navbar';
 
-
-function App() {
-  return (
-    <div className="App">
-    <h1>Hello from React</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Navbar />
+      </div>
+    );
+  }
 }
-
 export default App;
