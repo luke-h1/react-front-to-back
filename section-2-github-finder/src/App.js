492ee7ac3b0d373d2cf95eeff1c5bfafe644c97c
luke-h1 <luke.howsam@yahoo.com>
2020-09-11 21:34:07 +0100

    cleanup CRA

--- section-2-github-finder/src/App.js
+++ section-2-github-finder/src/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App' style={{ textAlign: 'center' }}>
+      Hello 🌍
+    </div>
+  );
 }
 
 export default App;
