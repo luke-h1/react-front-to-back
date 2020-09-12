516068830f902110b466fd8edb4a4f1e1c3adde0
luke-h1 <luke.howsam@yahoo.com>
2020-09-12 18:24:48 +0100

    update

--- section-2-github-finder/src/App.js
+++ section-2-github-finder/src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className='App' style={{ textAlign: 'center' }}>
-      Hello 🌍
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <h1>Hello</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
