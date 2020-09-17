55c48e669ea222292c5d3fe313e162c1e0338c77
luke-h1 <luke.howsam@yahoo.com>
2020-09-17 17:55:09 +0100

    add project-1

---
+++ project-1-github-finder/src/components/layout/Loading.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+
+const Loading = () => {
+  return (
+    <Fragment>
+      <img
+        src='https://media.giphy.com/media/zPOErRpLtHWbm/giphy.gif'
+        alt='loading ...'
+        style={{ width: '200px', margin: 'auto', display: 'block' }}
+      />
+    </Fragment>
+  );
+};
+export default Loading;
