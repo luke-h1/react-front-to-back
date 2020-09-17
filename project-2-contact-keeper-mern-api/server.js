b8d0f1f81cb3dfb453860308a470eed54cbd272b
luke-h1 <luke.howsam@yahoo.com>
2020-09-17 19:06:42 +0100

    use local mongodb instead of atlas

--- project-2-contact-keeper-mern-api/server.js
+++ project-2-contact-keeper-mern-api/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+connectDB(); // connect to local mongodb
+
 
 app.get('/', (req, res) =>
   res.json({ msg: 'Welcome to the contact keeper API 🍺 🎲 🚀' })
