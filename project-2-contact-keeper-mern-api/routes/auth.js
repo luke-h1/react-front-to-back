3ec127aa0944b658fbab5fdbe241646579763421
luke-h1 <luke.howsam@yahoo.com>
2020-09-18 10:19:40 +0100

    Auth Route & Login Route setup

--- project-2-contact-keeper-mern-api/routes/auth.js
+++ project-2-contact-keeper-mern-api/routes/auth.js
@@ -1,21 +1,63 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
+const User = require('../models/User');
 
 // @route   GET    api/auth
-// @desc    Get logged in user 
+// @desc    Get logged in user
 // @access  Private
 router.get('/', (req, res) => {
   res.send('Get logged in user ! 😀');
 });
 
-module.exports = router;
-
-
 // @route   POST    api/auth
-// @desc    Auth user & get token  
+// @desc    Auth user & get token
 // @access  Public
-router.post('/', (req, res) => {
-  res.send('Log in user 😀');
-});
+router.post(
+  '/',
+  [
+    check('email', 'Include a valid email').isEmail(),
+    check('password', 'Password is required').exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ msg: 'Invalid Email' });
+      }
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ msg: 'Invalid Password' });
+      }
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 36000000, // set back to 3600 (1 hour)
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
--- project-2-contact-keeper-mern-api/routes/users.js
+++ project-2-contact-keeper-mern-api/routes/users.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
-const { check, validationResult } = require('express-validator/check');
+const config = require('config');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
 // @route   POST    api/users
 // @desc    Register a user
 // @access  Public
@@ -16,12 +19,45 @@ router.post(
       }
     ),
   ],
-  (req, res) => {
-    const errors = validationResult(req);
+  async (req, res) => {
+    const errors =  validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send('User Created ! ✅');
+    const { name, email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({ msg: 'User already exists ' });
+      }
+      user = new User({
+        name,
+        email,
+        password,
+      });
+      const salt = await bcrypt.genSalt(15);
+      user.password = await bcrypt.hash(password, salt); // hash the password
+      await user.save(); // store new hashed password in Mongo
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 36000000, // set back to 3600 (1 hour)
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(`ERROR: ${err.message}`);
+      res.status(500).send('Server Error');
+    }
   }
 );
 
