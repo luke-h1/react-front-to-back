48ce2030c2e0b5ef7893e59186d329166a69dd62
luke-h1 <luke.howsam@yahoo.com>
2020-09-19 17:27:27 +0100

    add cra & navbar

---
+++ project-2-contact-keeper-mern-api/client/src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+      <p className="my-1">This is a full stack react app for keeping contacts.</p>
+      <p class="bg0dark p">Tech Used: MongoDB, express, react</p>
+    </div>
+  )
+}
+export default About 
