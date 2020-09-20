1ff939f56798a52e62963800ee3ecd3ed22f247c
luke-h1 <luke.howsam@yahoo.com>
2020-09-20 16:55:28 +0100

    loadUser()

--- project-2-contact-keeper-mern-api/client/src/components/pages/About.js
+++ project-2-contact-keeper-mern-api/client/src/components/pages/About.js
@@ -4,7 +4,7 @@ export const About = () => {
   return (
     <div>
       <p className="my-1">This is a full stack react app for keeping contacts.</p>
-      <p class="bg0dark p">Tech Used: MongoDB, express, react</p>
+      <p className="bg0dark p">Tech Used: MongoDB, express, react</p>
     </div>
   )
 }
